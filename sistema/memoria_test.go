4linux/202-go-labs

sistema: Add funcoes para coleta de memoria
Adcionar funcoes para lidar como arquivo /proc/meminfo, por padrao,
para expor e coletar informacoe de memoria do sistema atual.

Este commit adiciona as segunites funcionalidades:

  * Estrutura InfoMemoria{} com campos relevantes para calculo
    de utilizacao.

  * GetInfoMemoria(arquivo) - Extrair informacoes relevantes,
    para o pacote sistema, para realizar calculo de metricas
    de utilizacao de memoria. A funcao extrai: MemTotal,
    MemFree, Cached e Buffers.

  * GetInfoMemoriaDefault() - Executa GetInfoMemoria() passando o
    arquivo padrao: /proc/meminfo.

  * MemUso() - Retorna a utilizacao de memoria em porcetagem. Calculando
    o total, memoria disponivel, memoria de "buffer" e "cache".

sistema/memoria_test.go
@@ -1,0 +1,36 @@
+package sistema
+
+import (
+	"testing"
+)
+
+func TestarMemUso(t *testing.T) {
+	uso := MemUso()
+	if uso < 0 {
+		t.Errorf("MemUso() retornou um valor negavito (%f)", uso)
+	}
+}
+
+func TestarParseDeMemoria(t *testing.T) {
+	info := GetInfoMemoriaDefault()
+	if info.Total <= 0 {
+		t.Errorf("info.Total retornou zero ou negativo (%d)", info.Total)
+	}
+	if info.Buffered <= 0 {
+		t.Errorf("info.Buffered retornou zero ou negativo (%d)", info.Buffered)
+	}
+	if info.Cached <= 0 {
+		t.Errorf("info.Cached retornou zero ou negativo (%d)", info.Total)
+	}
+	if info.Free <= 0 {
+		t.Errorf("info.Free retornou zero ou negativo (%d)", info.Total)
+	}
+}
+
+func TestarUsoPerct(t *testing.T) {
+	info := InfoMemoria{Total: 10, Free: 1, Buffered: 1, Cached: 1}
+	perc := info.UsoPerct()
+	if perc != 70 {
+		t.Errorf("info.UsoPerct() retornou %f mas deveria retornar 70", perc)
+	}
+}

raffs/go-labs

sistema/memoria_test.go
@@ -1,0 +1,36 @@
+package sistema
+
+import (
+	"testing"
+)
+
+func testarMemUso(t *testing.T) {
+	uso := MemUso()
+	if uso < 0 {
+		t.Errorf("MemUso() retornou um valor negavito (%f)", uso)
+	}
+}
+
+func testarParseDeMemoria(t *testing.T) {
+	info := GetInfoMemoriaDefault()
+	if info.Total <= 0 {
+		t.Errorf("info.Total retornou zero ou negativo (%d)", info.Total)
+	}
+	if info.Buffered <= 0 {
+		t.Errorf("info.Buffered retornou zero ou negativo (%d)", info.Buffered)
+	}
+	if info.Cached <= 0 {
+		t.Errorf("info.Cached retornou zero ou negativo (%d)", info.Total)
+	}
+	if info.Free <= 0 {
+		t.Errorf("info.Free retornou zero ou negativo (%d)", info.Total)
+	}
+}
+
+func testarUsoPerct(t *testing.T) {
+	info := InfoMemoria{Total: 10, Free: 1, Buffered: 1, Cached: 1}
+	perc := info.UsoPerct()
+	if perc != 70 {
+		t.Errorf("info.UsoPerct() retornou %f mas deveria retornar 70", perc)
+	}
+}